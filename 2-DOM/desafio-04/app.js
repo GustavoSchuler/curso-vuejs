7b78fc09d0e386e7a163435e92dce87f16c8f813
Luis Gustavo Bier Schuler <luis.schuler@zenvia.com>
2020-04-21 22:01:00 -0300

    updating: desafio-04

--- 2-DOM/desafio-04/app.js
+++ 2-DOM/desafio-04/app.js
@@ -1,14 +1,45 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		class1: 'destaque',
+		userClass: 'sample',
+		userClass2: 'sample2',
+		sample3: true,
+		userStyle: 'background-color: red; width: 100px; height: 100px',
+		progress: 0,
+		pgBar: ''
 	},
 	methods: {
 		iniciarEfeito() {
-
+			setInterval(() => {
+				this.class1 = this.class1 == 'destaque' ? 'encolher' : 'destaque'
+			}, 2000)
 		},
 		iniciarProgresso() {
-
+			this.pgBar = setInterval(() => {
+				this.progress += 1
+			}, 100)
+		},
+		setSample3 (e) {
+			if (e.target.value == 'true') {
+				this.sample3 = true
+			} else if (e.target.value == 'false') {
+				this.sample3 = false
+			}
+		}
+	},
+	watch: {
+		progress (value) {
+			if (value == 100) clearInterval(this.pgBar)
+		}
+	},
+	computed: {
+		pgBarProgress () {
+			return {
+				backgroundColor: 'grey',
+				height: '20px',
+				width: `${this.progress}%`,
+			}
 		}
 	}
 })
